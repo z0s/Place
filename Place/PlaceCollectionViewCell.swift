e7cfadfebc5ded08ff7c294f37d8a48fe38b3454
Zubair <zubair@Zubairs-MacBook-Air.local>
2017-06-03 00:41:04 -0700

    Feat: Set placeholder image after setting place image view to nil.

--- Place/PlaceCollectionViewCell.swift
+++ Place/PlaceCollectionViewCell.swift
@@ -29,7 +29,6 @@ class PlaceCollectionViewCell: UICollectionViewCell {
         placeImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         placeImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
         placeImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        placeImageView.image = #imageLiteral(resourceName: "missing-image")
         
     }
 
@@ -44,6 +43,7 @@ class PlaceCollectionViewCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         placeImageView.image = nil
+        placeImageView.image = #imageLiteral(resourceName: "missing-image")
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
